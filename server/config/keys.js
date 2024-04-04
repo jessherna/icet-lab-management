18b9047b4492cde9552f1adc835465e81cc5566f
Jessica Hernandez <jessicamariep.hernandez@gmail.com>
2024-04-04 18:49:51 -0400

    server initialization

---
+++ server/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb://icetclass:Seta5b1pa55@15.156.204.35:27017/",
+}
---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser'); 
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
+
+// Database Configuration
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('Successfully connected to MongoDB!'))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
