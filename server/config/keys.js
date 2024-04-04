e39bf2c988b375e9e308c0c333d2e548f8382dc3
Jessica Hernandez <jessicamariep.hernandez@gmail.com>
2024-04-04 19:17:43 -0400

    finished backend for user management

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
     mongoURI: "mongodb://icetclass:Seta5b1pa55@15.156.204.35:27017/",
+    secretOrKey: "secret"
 }
--- server/server.js
+++ server/server.js
@@ -21,6 +21,15 @@ mongoose
     .then(() => console.log('Successfully connected to MongoDB!'))
     .catch(err => console.log(err));
 
+// Passport Middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
+
+// Routes
+app.use('/api/users', users);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
